572eeef3e9412a2462b4bf571cb728f670ac89c2
曹子旭 <73403611+Starkxim@users.noreply.github.com>
2025-02-20 16:33:46 +0800

    更新.gitignore以忽略/tests/test_combined_file.xlsx；修正test_ParseJson.py中的模块导入路径

--- tests/test_ParseJson.py
+++ tests/test_ParseJson.py
@@ -6,7 +6,7 @@ import os
 import requests
 
 # 确保可以导入 DataProcess 模块
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from DataProcess.ParseJson import get_holidays
 
