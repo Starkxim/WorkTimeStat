a527788de384e9d1e45eed1152188df03a178913
曹子旭 <73403611+Starkxim@users.noreply.github.com>
2025-02-20 16:42:20 +0800

    更新.flake8以忽略tests/test_ParseJson.py中的E402错误；确保在test_ParseJson.py中正确导入DataProcess模块

--- tests/test_ParseJson.py
+++ tests/test_ParseJson.py
@@ -4,10 +4,10 @@ from unittest.mock import MagicMock, patch
 import sys
 import os
 import requests
-from DataProcess.ParseJson import get_holidays
 
 # 确保可以导入 DataProcess 模块
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from DataProcess.ParseJson import get_holidays
 
 
 class TestParseJson(unittest.TestCase):
